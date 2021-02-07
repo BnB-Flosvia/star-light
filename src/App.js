f1daa2a35a538abd0a3f4cf673cb29659d9d8375
suhotang <dkdudkc22@gmail.com>
2021-02-07 14:15:03 +0900

    Ing   - Initial setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { ConnectedRouter } from "connected-react-router"
+import RoutePages from "./RoutePages"
 
-function App() {
+function App({ history }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ConnectedRouter history={history}>
+      <RoutePages />
+    </ConnectedRouter>
+  )
 }
 
-export default App;
+export default App
