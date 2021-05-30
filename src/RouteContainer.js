e4c7811a9f43da604d945ffa6dafc63d2ebebc0e
ewmango <ewmango@villanema.com>
2021-05-30 17:58:11 +0900

    Done - Add Portal component, Add new textTheme, Create TagSelectInput

--- src/RouteContainer.js
+++ src/RouteContainer.js
@@ -1,13 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from "react"
 import styled, { createGlobalStyle } from "styled-components"
+import { PORTAL_ELEMENT_ID } from "components/Portal"
 import { Redirect, Route } from "react-router-dom"
+import NavigationBar from "components/NavigationBar"
+import PerfectScrollbar from "react-perfect-scrollbar"
+import Header from "components/Header"
+import Footer from "components/Footer"
+import { checkLocalToken } from "authProvider"
 
 const GlobalStyle = createGlobalStyle`
-  html {
-    min-width: 320px;
-  }
-  
   * {
     user-select: none;
   }
@@ -29,15 +31,20 @@ const Container = styled.div`
   padding: 0;
   display: flex;
   flex-direction: column;
+  min-width: 340px;
 `
 
 const ContentContainer = styled.div`
   display: flex;
-  flex-flow: row nowrap;
-  overflow: hidden;
+  flex-flow: column;
   width: 100%;
   flex: 1;
-  position: relative;
+`
+
+const ScrollContainer = styled(PerfectScrollbar)`
+  display: flex;
+  flex-flow: column;
+  flex: 1;
 `
 
 export default function RouteContainer({
@@ -45,16 +52,24 @@ export default function RouteContainer({
   fullSize,
   children,
   component,
+  requiredLogin,
   ...props
 }) {
   const childComponent = children || component || <div />
 
-  const isAllowed = true
+  const isAllowed = !requiredLogin || checkLocalToken()
+
+  const onScroll = (event) => {
+    const scrollTop = event?.currentTarget?.scrollTop
+
+    // TODO: set navigation bar position to fix
+    if (scrollTop) console.log(scrollTop)
+  }
 
   if (!isAllowed) {
     return (
       <Route path={path} {...props}>
-        <Redirect to="/main" />
+        <Redirect to="/signin" />
       </Route>
     )
   }
@@ -62,17 +77,20 @@ export default function RouteContainer({
   return (
     <Route path={path} {...props}>
       <GlobalStyle />
-      {/* <div id={POPUP_PORTAL_ELEMENT_ID} /> */}
+      <div id={PORTAL_ELEMENT_ID} />
       <Container>
-        {fullSize ? (
-          childComponent
-        ) : (
-          <>
-            {/* TODO: Header, Menu bar */}
-            <ContentContainer>{childComponent}</ContentContainer>
-            {/* TODO: Footer */}
-          </>
-        )}
+        <ScrollContainer onScroll={onScroll}>
+          {fullSize ? (
+            childComponent
+          ) : (
+            <>
+              <Header />
+              <NavigationBar isFixed={false} />
+              <ContentContainer>{childComponent}</ContentContainer>
+              <Footer />
+            </>
+          )}
+        </ScrollContainer>
       </Container>
     </Route>
   )
