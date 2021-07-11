65a72aefa8315d4c8696fba47efe2fdbb147f5bb
hanaldo <jiw021538@gmail.com>
2021-07-11 22:12:48 +0900

    Fix: no copy build to app and contain build dir to deploy package

--- .platform/hooks/prebuild/00_diable_npm.sh
+++ .platform/hooks/prebuild/00_diable_npm.sh
@@ -6,5 +6,5 @@
 # Note that this *overwrites* Elastic Beanstalk's default 50npm.sh script
 # (https://gist.github.com/wearhere/de51bb799f5099cec0ed28b9d0eb3663).
 
-# mv /var/app/staging/package.json /var/app/staging/package.json.tmp
-# mv /var/app/staging/package-lock.json /var/app/staging/package-lock.json.tmp
+mv /var/app/staging/package.json /var/app/staging/package.json.tmp
+mv /var/app/staging/package-lock.json /var/app/staging/package-lock.json.tmp
